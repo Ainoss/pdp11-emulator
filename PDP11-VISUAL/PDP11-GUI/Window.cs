138a945d9ff3f8b285a88be52c07744d51db5953
Daniil Sargin <avterks@gmail.com>
2016-01-21 01:27:30 +0300

    add image and some work in window.cs

--- PDP11-VISUAL/PDP11-GUI/Window.cs
+++ PDP11-VISUAL/PDP11-GUI/Window.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,22 @@ namespace PDP11_GUI
         static extern uint pdp11_init(IntPtr prt);
         [DllImport("PDP11-EMULATOR.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern uint pdp11_debug();
+        [DllImport("PDP11-EMULATOR.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern uint pdp11_disasm(UInt16[] ppc);
+
+        // Screen
+        static Rectangle screenBorders = new Rectangle(0, 0, 100, 100);
+        Bitmap screenImage = new Bitmap(screenBorders.Width, screenBorders.Height, PixelFormat.Format1bppIndexed);
+        IntPtr videoMemory = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(byte)) * screenBorders.Width * screenBorders.Height / 8);
 
         public Window()
         {
             InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            Screen.Image = screenImage;
         }
 
         private void stepButton_Click(object sender, EventArgs e)
